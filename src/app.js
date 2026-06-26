c60d0e6145c8e6759aff737cbf834f1cc6b5e973
helium-ops <mohamedmah1234@outlook.com>
2026-06-26 00:31:48 -0400

    Added a new question

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
-const question = document.querySelector(".question");
-const answerBtn = document.querySelector(".buttons button");
+const questionText = document.querySelector(".question");
+const answerBtn = document.querySelectorAll(".buttons button");
 const nextBtn = document.querySelector(".next");
 
-function displayQuestion(){
-  let questions = {
-    question = "What is the capital of the United States",
-    answers = ["Berlin", "Madrid", "New York City", "Washington D.C"],
-    correctAnswer = question.answers[3]
-  }
-  question.innerHTML = questions.question;
-  
-}
+const displayQuestion = () => {
+  const question = {
+    question: "What is the capital of the United States?",
+    answers: ["Berlin", "Madrid", "New York City", "Washington D.C"],
+    correctAnswer: "Washington D.C"
+  };
+
+  questionText.innerHTML = question.question;
+
+  answerBtn.forEach((btn, index) => {
+    btn.innerHTML = question.answers[index];
+  });
+};
 
 displayQuestion();
